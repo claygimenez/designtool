7fe11655985eb861ef7e0aa0be42ac85ce7e6e83
Clay Gimenez <claygimenez@gmail.com>
2014-07-13 09:58:53 -0400

    wip

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ActiveRecord::Base
+  require 'treat'
+
   belongs_to :user
   has_many :notes
 
@@ -7,21 +9,29 @@ class Project < ActiveRecord::Base
   end
 
   def reflect
+    insights = Hash.new
+    notewords = []
     frequencies = Hash.new(0)
+
     self.notes.each do |note|
-      notewords = note.text.split(" ")
-      notewords.each do |noteword|
-        unless !!(noteword =~ /\b(is|it|for|this|that|with|in|have|was|the|and|i|on|are|of|my|but|can|you|a|from|to|as|not|be|like|very|had|one|so|or|has|really|many|just|all|would|if|than|more|at|no|because)\b/i)
-          frequencies[noteword] += 1
-        end
-      end
+      # insights[note.id] = {:raw => note.text}
+      scrubNote = note.text.to_entity
+      insights[note.id] = {:treatobj => scrubNote}
+      scrubNote.do(:chunk, :segment, :tokenize, :parse)
+      scrubWords = []
+      scrubNote.words.each { |word| scrubWords.push(word.to_s) }
+      insights[note.id]['words'] = scrubWords
+      notewords.concat(scrubWords)
     end
+
+    notewords.each { |noteword| frequencies[noteword] += 1 }
 
     frequencies = frequencies.sort_by {|k,v| v}
     frequencies.reverse!
 
     reflections = []
     unless frequencies.blank?
+      # reflections.push('hi')
       10.times do |w|
         word = frequencies[w][0]
         value = frequencies[w][1]
